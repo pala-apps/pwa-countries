8b87b92acdc232f00a1bdfe0862dc530fa4f7dac
Richard Henry <r.henry1989@gmail.com>
2016-08-29 16:30:11 +0100

    trying

--- app/assets/sw/sw-toolbox-config.js
+++ app/assets/sw/sw-toolbox-config.js
@@ -2,6 +2,40 @@ toolbox.router.get( '/rest/v1/all', toolbox.fastest, {
   origin: /^https:\/\/restcountries.eu/
 });
 
-toolbox.router.get( '/flags/(.*)', toolbox.fastest, {
-  origin: /^https:\/\/www.countries-ofthe-world.com/
+var MISSING_IMAGE = '/images/placeholder.png';
+// toolbox.cache( { la: MISSING_IMAGE } );
+toolbox.precache([MISSING_IMAGE])
+// caches.add( MISSING_IMAGE )
+// console.log('toolbox', toolbox)
+
+function imageHandler( request, values, options ) {
+  // return new Response()
+  // return caches.match( MISSING_IMAGE )
+  //
+
+  request.mode = 'cors'
+
+  console.log('request', request)
+  //
+  return fetch(request.clone()).then(function(response){
+    console.log('response', response)
+    return response
+  })
+
+  // return toolbox.cacheFirst( request, values, options )
+  // .then( function( res, second ) {
+  //   console.log( "request nOnly", res )
+  //   return caches.match( MISSING_IMAGE )
+  // })
+  // .catch( function(res) {
+  //   console.log('in catch YO', res)
+  //   return caches.match( MISSING_IMAGE )
+  // })
+}
+
+toolbox.router.get( '/flags/(.*)', imageHandler, {
+  cache: { name: 'image-cache' },
+  origin: /^https:\/\/www.countries-ofthe-world.com$/,
+  successResponses: /200$/,
+  debug: true
 })
