4925c3962302689531d13bd03b7ec1b608871cbe
Richard Henry <r.henry1989@gmail.com>
2016-08-30 21:23:40 +0100

    adding in cacheFirst for fonts

--- app/assets/sw/sw-toolbox-config.js
+++ app/assets/sw/sw-toolbox-config.js
@@ -3,5 +3,9 @@ toolbox.router.get( '/rest/v1/all', toolbox.fastest, {
 });
 
 toolbox.router.get( '/images/flags/(.*)', toolbox.cacheFirst, {
-  cache: { name: 'flag-cache' },
+  cache: { name: 'flag-cache' }
+})
+
+toolbox.router.get( '/fonts/(.*)', toolbox.cacheFirst, {
+  cache: { name: 'font-cache' }
 })
